1df3a07984cdc18d6ac27d1fca77151066fa3693
DevGBX9 <DevGBX9@users.noreply.github.com>
2026-08-02 05:17:27 -0700

    Drop Fabric support; single Bukkit/Spigot/Paper plugin

---
+++ src/main/java/com/devgbx9/mineflayer/Mineflayer.java
@@ -0,0 +1,14 @@
+package com.devgbx9.mineflayer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Mineflayer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
